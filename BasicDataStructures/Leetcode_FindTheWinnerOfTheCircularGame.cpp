1c973902596ff4783aac73258e03bce73df938de
Eduardo Jesus Dal Pizzol <eduardo.pizzol@colaboradores.mcti.gov.br>
2026-08-10 13:49:02 -0300

    add: another problem

---
+++ BasicDataStructures/Leetcode_FindTheWinnerOfTheCircularGame.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findTheWinner(int n, int k){
+    queue<int> q;
+    for(int i=1;i<=n;i++) q.push(i);
+
+    while(q.size()>1){
+        for(int i=0;i<k-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
+        q.pop();
+    }
+
+    return q.front();
+}
+
+int main(){
+
+    int n,k;
+    cin>>n>>k;
+    cout<<findTheWinner(n,k)<<endl;
+
+}
