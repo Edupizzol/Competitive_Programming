fe2635c7f7e2df6e90af616edb352fb022acabe6
Edupizzol <edupizzol16@gmail.com>
2026-08-19 08:26:34 -0300

    add: bfs template

---
+++ Templates/BFS.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<bool> visited;
+vector<int> distances;
+
+void bfs(int origem){
+    queue<int> q;
+    q.push(origem);
+    visited[origem]=true;
+    distances[origem]=true;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        
+        for(int neighbour:adj[node]){
+            if(!visited[neighbour]){
+                visited[neighbour]=true;
+                distances[neighbour]=distances[node]+1;
+                q.push(neighbour);
+            }
+        }
+    }
+
+    return;
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+
+    adj.assign(n+1, {});
+    visited.assign(n+1, false);
+    distances.assign(n+1, -1);
+
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); //se o grafo for direcionado obviamente apaga isso
+    }
+
+    bfs(1);
+}
